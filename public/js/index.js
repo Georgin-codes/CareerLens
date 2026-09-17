0d5fa82c52649d6d53679418ab77f3aa7b99a3e5
Georgin-codes <georginp.tecjob@gmail.com>
2026-09-17 14:50:52 -0500

    rebuild index page styling

--- public/js/index.js
+++ public/js/index.js
@@ -66,7 +66,7 @@ forgotPassword.addEventListener("click", async ()=>{
     title.style.color = "#AFC0A3"
     description.style.color = "#C8D0C5"
     forgotPassword.disabled = true;
-    apiResponseCont.style.display = "flex"
+    apiResponseCont.style.display = "block"
 
     const email = document.getElementById('email').value.trim()
     if(!email || !email.includes('@')){
